8ebad94d5e72404f99e3a164f582e7fe021dd253
Dimitri Lahaye <contact@dimitrilahaye.net>
2022-05-13 13:41:00 +0200

    arbo

--- src/core/boundingContexts/priceBreakdown/modules/contract/domain/shared/types.ts
+++
@@ -1,5 +0,0 @@
-import Agency from "../entities/agency";
-import Carrier from "../entities/carrier";
-import Provider from "../entities/provider";
-
-export type Actor = Agency | Provider | Carrier;
---
+++ src/core/boundingContexts/priceBreakdown/modules/contract/shared/types.ts
@@ -0,0 +1,5 @@
+import Agency from "../domain/entities/agency";
+import Carrier from "../domain/entities/carrier";
+import Provider from "../domain/entities/provider";
+
+export type Actor = Agency | Provider | Carrier;
